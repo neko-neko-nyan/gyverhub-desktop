d2267bee26c4fa5ce7e48f9a2f056083c7a32d3a
NekoNekoNyan <me@neko-dev.ru>
2024-01-16 17:05:20 +0300

    fix detection update builtin hub app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@ use url::Url;
 use std::{fs, fs::File, path::Path, io};
 
 const VERSION_URL: &str = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/version.txt";
-const UPDATE_URL: &str = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/local/GyverHub.html";
+const UPDATE_URL: &str = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/app/index.html";
 
 fn get_online_version() -> reqwest::Result<String> {
     Ok(reqwest::blocking::get(VERSION_URL)?.text()?)
@@ -51,6 +51,34 @@ fn check_updates(version_path: &Path, gh_path: &Path) -> bool {
     }
 }
 
+fn target_arch() -> &'static str {
+    if cfg!(target_arch = "x86") {
+        "i686"
+    } else if cfg!(target_arch = "x86_64") {
+        "x86_64"
+    } else if cfg!(target_arch = "arm") {
+        "armv7"
+    } else if cfg!(target_arch = "aarch64") {
+        "aarch64"
+    } else {
+        "unknown"
+    }
+}
+
+fn target_os() -> &'static str {
+    if cfg!(target_os = "linux") {
+        "linux"
+    } else if cfg!(target_os = "macos") {
+        "macos"
+    } else if cfg!(target_os = "windows") {
+        "windows"
+    } else if cfg!(target_os = "freebsd") {
+        "freebsd"
+    } else {
+        "unknown"
+    }
+}
+
 fn main() {
     let mut ctx: Context<EmbeddedAssets> = tauri::generate_context!();
     let cfg: &mut tauri::Config = ctx.config_mut();
@@ -72,9 +100,12 @@ fn main() {
         cfg.build.dist_dir = AppUrl::Url(WindowUrl::External(url));
     }
 
+    let desktop_version = cfg.package.version.clone().unwrap();
+
     tauri::Builder::default()
-        .on_page_load(|win, _payload| {
-            let _ = win.eval("window.__desktop__=!0;window.isLocal=()=>!0;");
+        .on_page_load(move |win, _payload| {
+            let code = format!("window.GyverHubDesktop={{version:'{}',arch:'{}',os:'{}',debug:{}}};", desktop_version, target_arch(), target_os(), if cfg!(dev) {"true"} else {"false"});
+            let _ = win.eval(&code);
 
             #[cfg(not(dev))]
             let _ = win.eval(
